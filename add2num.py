08a9f268f778986cb5ea087999edf846ebabfed2
Siddhart <Siddharth.pandey1@gmail.com>
2025-08-09 21:38:20 +0530

    addition_working

---
+++ add2num.py
@@ -0,0 +1,9 @@
+def add_two_numbers(a, b):
+    return a + b
+
+# Example usage
+if __name__ == "__main__":
+    num1 = float(input("Enter first dhruv number: "))
+    num2 = float(input("Enter aditya number: "))
+    result = add_two_numbers(num1, num2)
+    print("The sum is:", result)
